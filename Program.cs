f2f8434361b7df5ed0416c26d76be88c603c935b
Bhavik Vora <bhavik.vora@microsoft.com>
2018-11-29 16:18:53 -0800

    Update as Amazon had made some updates

--- Program.cs
+++ Program.cs
@@ -2,74 +2,88 @@
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
+using System.Linq;
 using System.Threading;
 
 namespace AmazonGiftCardsBuy
 {
-    class Program
-    {
-        private const double GIFT_CARD_AMOUNT = 0.5;
-        private const string AMAZON_USERNAME = "";
-        private const string AMAZON_PASSWORD = ""; 
-        private const string DEBIT_CARD_NUMBER = ""; // Update debit card info here
-        private const int NUMBER_OF_RELOADS = 2; // Update the number of reloads to be done with this card
+	class Program
+	{
+		private const double GIFT_CARD_AMOUNT = 0.5;
+		private const string AMAZON_USERNAME = "";
+		private const string AMAZON_PASSWORD = ""; 
+		private const string CARD_NUMBER = ""; // Update card info here
+		private const string CARD_TYPE = "Debit Card"; // Options: Visa, Debit Card, American Express, etc
+		private const int NUMBER_OF_RELOADS = 2; // Update the number of reloads to be done with this card
 
-        static void Main(string[] args)
-        {
-            var driver = new ChromeDriver();
-            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            
-            // Sign in
-            driver.Navigate().GoToUrl("https://www.amazon.com/asv/reload/");
-            driver.FindElementById("form-submit-button").Click();
-            wait.Until(d => d.Title == "Amazon.com Sign In");
-            driver.FindElementById("ap_email").SendKeys(AMAZON_USERNAME);
-            driver.FindElementById("ap_signin_existing_radio").Click();
-            driver.FindElementById("ap_password").SendKeys(AMAZON_PASSWORD);
-            driver.FindElementById("signInSubmit").Click();
-            Console.WriteLine("Signed in successfully!");
+		static void Main(string[] args)
+		{
+			var last4CardDigits = CARD_NUMBER.Substring(CARD_NUMBER.Length - 4);
+			string reloadUrl = "https://smile.amazon.com/asv/reload/";
 
-            // Loop and buy gift cards
-            for (int i = 0; i < NUMBER_OF_RELOADS; i++)
-            {
-                Console.WriteLine("Iteration 1 ------ ");
+			// now automate using Selenium
+			var driver = new ChromeDriver();
+			var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+			
+			// Sign in
+			driver.Navigate().GoToUrl(reloadUrl);
+			driver.FindElementById("form-submit-button").Click();
+			wait.Until(d => d.Title == "Amazon Sign In");
+			driver.FindElementById("ap_email").SendKeys(AMAZON_USERNAME);
+			//driver.FindElementById("ap_signin_existing_radio").Click();
+			driver.FindElementById("ap_password").SendKeys(AMAZON_PASSWORD);
+			driver.FindElementById("signInSubmit").Click();
+			
+			Console.WriteLine("Signed in successfully! Waiting for 60 seconds for allowing to allow user input for 2FA");
+			Thread.Sleep(60000);
 
-                if (driver.Title != "Reload Your Balance")
-                {
-                    driver.Navigate().GoToUrl("https://www.amazon.com/asv/reload/");
-                }                
-                wait.Until(d => d.Title == "Reload Your Balance");
-                
-                driver.FindElementById("asv-manual-reload-amount").SendKeys(GIFT_CARD_AMOUNT.ToString());
-                Thread.Sleep(1000);
+			// Loop and buy gift cards
+			for (int i = 0; i < NUMBER_OF_RELOADS; i++)
+			{
+				Console.WriteLine($"Iteration {i} ------ ");
 
-                driver.FindElementByXPath("//span[contains(text(), 'Debit Card')]").Click();
-                if(i == 0)
-                {
-                    driver.FindElementByName("addCardNumber").SendKeys(DEBIT_CARD_NUMBER);
-                    driver.FindElementByXPath("//button[contains(.,'Confirm Card')]").Click();
-                    Thread.Sleep(1000);
-                }
+				if (driver.Title != "Reload Your Balance")
+				{
+					driver.Navigate().GoToUrl(reloadUrl);
+				}                
+				wait.Until(d => d.Title == "Reload Your Balance");
+				
+				driver.FindElementById("asv-manual-reload-amount").SendKeys(GIFT_CARD_AMOUNT.ToString());
+				Thread.Sleep(100);
 
-                driver.FindElementById("form-submit-button").Click();
-                Thread.Sleep(1000);
+				driver.FindElementByXPath($"//span[contains(text(), '{CARD_TYPE}')]").Click();
+				//driver.FindElementByName("pmts-credit-card-row")[2].click()
+				Thread.Sleep(100);
+				if(i == 0)
+				{
+					driver.FindElementByXPath($"//input[@placeholder='ending in {last4CardDigits}']").SendKeys(CARD_NUMBER);
+					Thread.Sleep(100);
+					var elements = driver.FindElementsByXPath("//button[contains(.,'Confirm Card')]");
+					elements.First(x => x.Displayed).Click();
+					Thread.Sleep(100);
+				}
 
-                try
-                {
-                    driver.FindElementByXPath("//span[contains(.,'this message again')]").Click();
-                    Thread.Sleep(1000);
-                    driver.FindElementById("asv-reminder-action-primary").Click();
-                    Thread.Sleep(1000);
-                }
-                catch(NoSuchElementException)
-                {
-                    // do nothing
-                }
+				driver.FindElementById("form-submit-button").Click();
+				Thread.Sleep(1000);
 
-                driver.Navigate().GoToUrl("https://www.amazon.com/asv/reload/");
-            }
+				try
+				{
+					driver.FindElementByXPath("//span[contains(.,'this message again')]").Click();
+					Thread.Sleep(100);
+					driver.FindElementById("asv-reminder-action-primary").Click();
+					Thread.Sleep(100);
+				}
+				catch(NoSuchElementException)
+				{
+					// do nothing
+				}
 
-            driver.Quit();
-        }
-    }
+				driver.Navigate().GoToUrl(reloadUrl);
+			}
+
+			driver.Quit();
+
+			Console.Write("Done!!");
+		}
+	}
 }
--- README.md
+++ README.md
@@ -4,7 +4,8 @@ C# program to auto-buy Amazon gift cards
 
 Based off of: 
 https://github.com/justinjohnso/giftcards_galore
-https://github.com/ageoldpun/amazon_gifts 
+https://github.com/ageoldpun/amazon_gifts
+https://github.com/everettsouthwick/amazon-auto-reload
 
 
 ## Dependencies:
@@ -21,8 +22,9 @@ Open the C# project/sln and replace the following variables:
         private const double GIFT_CARD_AMOUNT = 0.5;
         private const string AMAZON_USERNAME = "";
         private const string AMAZON_PASSWORD = ""; 
-        private const string DEBIT_CARD_NUMBER = ""; // Update debit card info here
+        private const string CARD_NUMBER = ""; // Update debit card info here
+        private const string CARD_TYPE = "Debit Card"; // Options: Visa, Debit Card, American Express, etc
         private const int NUMBER_OF_RELOADS = 2; // Update the number of reloads to be done with this card
 
-
+Build the solution in VS and run!
 To be sure, I would start with low number of reloads.
